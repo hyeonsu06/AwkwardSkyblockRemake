daf26748f0c3aa681bea7ca98a9ab9e2841413a1
누리 <66936518+hyeonsu06@users.noreply.github.com>
2024-10-18 15:45:16 +0900

    fix

--- src/main/java/io/hyonsu06/core/VanillaManager.java
+++ src/main/java/io/hyonsu06/core/VanillaManager.java
@@ -1,6 +1,5 @@
 package io.hyonsu06.core;
 
-import org.bukkit.Bukkit;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
@@ -10,6 +9,8 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntitySpawnEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 import static io.hyonsu06.core.functions.setImmuneTime.setNoDamageTicks;
 
@@ -19,6 +20,24 @@ public class VanillaManager implements Listener {
         EntityType type = e.getEntity().getType();
         if (type.equals(EntityType.WITHER) || type.equals(EntityType.ENDER_DRAGON) || type.equals(EntityType.ELDER_GUARDIAN) || type.equals(EntityType.WARDEN)) {
             setNoDamageTicks((LivingEntity) e.getEntity(), 0);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.LOW)
+    public void onDamage(EntityDamageByEntityEvent e) {
+        EntityType type = e.getEntity().getType();
+        if (type.equals(EntityType.WITHER)) {
+            e.setDamage(e.getDamage() * 150);
+            ((LivingEntity) e.getEntity()).addPotionEffect(new PotionEffect(PotionEffectType.WITHER, 30, 0, false));
+        }
+        if (type.equals(EntityType.ENDER_DRAGON)) {
+            e.setDamage(e.getDamage() * 500);
+        }
+        if (type.equals(EntityType.ELDER_GUARDIAN)) {
+            e.setDamage(e.getDamage() * 80);
+        }
+        if (type.equals(EntityType.WARDEN)) {
+            e.setDamage(e.getDamage() * 10000);
         }
     }
 
@@ -39,7 +58,7 @@ public class VanillaManager implements Listener {
         }
         if (type.equals(EntityType.WARDEN)) {
             ((LivingEntity) e.getEntity()).getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(20000000);
-            ((LivingEntity) e.getEntity()).getAttribute(Attribute.GENERIC_ARMOR).setBaseValue(999);
+            ((LivingEntity) e.getEntity()).getAttribute(Attribute.GENERIC_ARMOR).setBaseValue(19);
         }
     }
 }
